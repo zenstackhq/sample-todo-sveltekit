1ec93819d912bd42be086381cb9ba9627af550e4
JG <jiashengguo@outlook.com>
2023-06-14 16:22:23 +0100

    fix: fix prisma error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
     plugins: [sveltekit()],
+    resolve: {
+        alias: {
+          ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
+        }
+      }
 };
 
 export default config;
