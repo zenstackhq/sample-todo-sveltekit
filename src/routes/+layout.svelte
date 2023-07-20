36553fabd078ba75e944b5a942776f4ef3eaf7c7
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-07-20 10:23:32 +0800

    fix ssr issue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+    import { browser } from '$app/environment';
     import { SvelteQueryContextKey } from '$lib/hooks';
     import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
     import { setContext } from 'svelte';
     import '../app.css';
 
-    const queryClient = new QueryClient();
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                // disable fetching on the server
+                enabled: browser,
+            },
+        },
+    });
     setContext(SvelteQueryContextKey, { endpoint: '/api/model' });
 </script>
 
--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,15 @@ const config = {
     plugins: [sveltekit()],
     resolve: {
         alias: {
-          ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
-        }
-      }
+            '.prisma/client/index-browser':
+                './node_modules/.prisma/client/index-browser.js',
+        },
+    },
+    ssr: {
+        // this is needed to make sure "vite" processes "@tanstack/svelte-query" imported inside
+        // "@zenstackhq/tanstack-query"
+        noExternal: ['@zenstackhq/tanstack-query'],
+    },
 };
 
 export default config;
